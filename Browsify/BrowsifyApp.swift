1036f4dcf25fd15fec87650304ee0af4701d94b4
Yumi Izumi <mail@yumi.to>
2025-10-14 06:11:51 +0900

    Initial Commit

---
+++ Browsify/BrowsifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowsifyApp.swift
+//  Browsify
+//
+//  Created by Yumi Izumi on 2025/10/14.
+//
+
+import SwiftUI
+
+@main
+struct BrowsifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
